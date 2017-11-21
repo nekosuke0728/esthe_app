288ee323d2d2e8f1cd7bd933609f68fcbfcc9cdd
nekosuke0728 <hatarakazarumono.kuubekarazu00@gmail.com>
2017-11-22 03:23:52 +0900

    devise ログイン機能実装（user,staff）

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+
+  devise_for :staffs, controllers: {
+    sessions:      'staffs/sessions',
+    passwords:     'staffs/passwords',
+    registrations: 'staffs/registrations'
+  }
+  devise_for :users, controllers: {
+    sessions:      'users/sessions',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations'
+  }
+
+  # devise_for :staffs
+  # devise_for :users
+
+  root 'user#top'
+  
   get 'staff/top'
 
   get 'user/top'
